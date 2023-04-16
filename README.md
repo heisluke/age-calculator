a120f89a132142d2a7bcb3dbd857a8ec0b67aaff
heisluke <lukebamtefa@gmail.com>
2023-04-16 08:37:58 +0100

    tryna host em

---
+++ README.md
@@ -0,0 +1,70 @@
+# Frontend Mentor - Age calculator app solution
+
+This is a solution to the [Age calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![Age Calculator Screen](./screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Desktop-first workflow
+- Vanilla Javascript
+
+### What I learned
+
+One of challenges I encountered while building this was making the ```new Date()``` take in the inputted value. It took me iterating in the console to find out that it only takes in th value if there's a `/` or a `-` seperating the values.
+
+```js
+let birthDate = new Date(userMonth+"-"+userDay+"-"+userYear);  
+
+let differenceInMilliseconds = currentDate.getTime() - birthDate.getTime();
+```
+
+### Useful resources
+
+- [Stack Overflow](https://www.stackoverflow.com/questions/9229213/convert-iso-date-to-milliseconds-in-javascript#:~:text=var%20date%20%3D%20new%20Date(date_string,This%20worked%20for%20me!) - This helped me understand how dates work in Javascript.
+
+## Author
+
+- Website - [Luke Bamtefa](https://luke-at.me)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
+- Twitter - [@yourusername](https://www.twitter.com/donotlukeatme)
---
+++ index.js
@@ -0,0 +1,75 @@
+/*input elements*/
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const convertAge = document.getElementById("convert-age");
+const dayFormError = document.querySelector(".day-form-error");
+const monthFormError = document.querySelector(".month-form-error");
+const yearFormError = document.querySelector(".year-form-error");
+const ghostStyler = document.querySelector("#ghost-styler")
+const currentDate = new Date();
+
+
+convertAge.addEventListener("click", ageCalc);
+/*this is cool, when will my stats sync?*/
+
+
+//let exmonth = [4,6,9,11];
+
+
+function ageCalc() {
+    if (day.value == "" || month.value == "" || year.value =="") {
+        dayFormError.textContent = "This field is required";
+        monthFormError.textContent = "This field is required";
+        yearFormError.textContent = "This field is required";
+        //the code below  send styles an empty span tag to show errors
+        ghostStyler.innerHTML = "<style> .top-label { color: hsl(0, 100%, 67%);} input[type=text]{border: 1px solid hsl(0, 100%, 67%);}  </style>"
+
+    } else if (day.value > 31 || month.value > 12 || year.value > currentDate.getFullYear()) {
+        dayFormError.textContent = "Must be a valid day";
+        monthFormError.textContent = "Must be a valid month";
+        yearFormError.textContent = "Must be in the past";
+        ghostStyler.innerHTML = "<style> .top-label { color: hsl(0, 100%, 67%);} input[type=text]{border: 1px solid hsl(0, 100%, 67%);}  </style>"
+        console.log("todays year is " + currentDate.getFullYear() );
+
+    } /*else if (day.value === 31 && month.value === 4) {
+        for (let i = 0; i < exMonth.length; i++) {
+            if (exMonth[i] === exMonth[i]) {
+                console.log("won't allow dude");
+            }
+        }
+        
+    }*/ else {
+        //idk a better way to reset the paragraph
+        dayFormError.textContent = "";
+        monthFormError.textContent = "";
+        yearFormError.textContent = "";
+        ghostStyler.innerHTML = "<style> .top-label { color: hsl(0, 1%, 44%);} input[type=text]{border: 1px solid hsl(0, 0%, 86%);}  </style>";
+
+        /*output elements*/
+        let yearsOutput = document.getElementById("years-output");
+        let monthsOutput = document.getElementById("months-output");
+        let daysOutput = document.getElementById("days-output");
+
+        /* user input handler */
+        let userDay = day.value;
+        let userMonth = month.value;
+        let userYear = year.value;
+
+        /*it workksss 😭😭😭😭, the new date() just needs the value to have "/" or "-" seperating it!*/
+        let birthDate = new Date(userMonth+"-"+userDay+"-"+userYear);  
+
+        let differenceInMilliseconds = currentDate.getTime() - birthDate.getTime();
+        
+        let millisecondsPerYear = 1000 * 60 * 60 * 24 * 365.25; //year answer
+        yearsOutput.textContent =  Math.floor(differenceInMilliseconds / millisecondsPerYear);
+
+        let millisecondsPerMonth = 1000 * 60 * 60 * 24 * 30.44; // average month length
+        monthsOutput.textContent = Math.floor((differenceInMilliseconds % millisecondsPerYear) / millisecondsPerMonth);
+
+        let millisecondsPerDay = 1000 * 60 * 60 * 24; // day answer
+        daysOutput.textContent = Math.floor((differenceInMilliseconds % millisecondsPerMonth) / millisecondsPerDay);
+    
+
+    }
+};
